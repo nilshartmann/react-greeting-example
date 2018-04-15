8e356bf8f0efed5842d89c9e66cfa54ce54d6729
Nils Hartmann <nils@nilshartmann.net>
2018-04-15 11:44:28 +0200

    Use BrowserRouter

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import GreetingController from "./GreetingController";
 import GreetingDisplayController from "./GreetingDisplayController";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Layout = () => (
   <Router>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,28 +1,29 @@
 const path = require("path");
 module.exports = {
-	entry: "./src/main.tsx",
-	output: {
-		path: __dirname + "/public/dist/",
-		filename: "main.js",
-		publicPath: "dist/"
-	},
-	mode: "development",
-	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(t|j)sx?$/,
-				use: "awesome-typescript-loader",
-				exclude: /node_modules/
-			},
-			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-		]
-	},
-	devtool: "inline-source-map",
-	devServer: {
-		contentBase: path.join(__dirname, "public/"),
-		port: 8080
-	}
+  entry: "./src/main.tsx",
+  output: {
+    path: __dirname + "/public/dist/",
+    filename: "main.js",
+    publicPath: "dist/"
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: "awesome-typescript-loader",
+        exclude: /node_modules/
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 8080,
+    historyApiFallback: true
+  }
 };
